18b3a36e91d55d3346042f03607a50dff4ceb275
Xavier Morin <xavier10@live.ca>
2020-10-11 21:21:43 -0400

    Output, Base and Best Tree

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Sklearn imports. Uncomment as you need, it gets long to run otherwise
-from sklearn.metrics import precision_recall_fscore_support  # f1 by default
-from sklearn.naive_bayes import GaussianNB
-# from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import precision_recall_fscore_support,confusion_matrix,accuracy_score  # f1 by default
+#from sklearn.naive_bayes import GaussianNB
+
+from sklearn.model_selection import GridSearchCV
 # from sklearn.linear_model import Perceptron
 # from sklearn.neural_network import MLPClassifier
 
@@ -43,8 +44,7 @@ def plot_data(data):
     plt.legend()
     plt.show()
 
-    return
-
+    
 
 def output_metrics_and_csv(y_pred,y_true,model_name,dataset_id):
     """ Description
@@ -64,18 +64,25 @@ def output_metrics_and_csv(y_pred,y_true,model_name,dataset_id):
     """
 
     # Get metrics TODO Clarify which are pertinent
-    precision_per_class, recall_per_class, f1_per_class = precision_recall_fscore_support(y_true, y_pred)
-    precision_weighted, recall_weighted, f1_weighted = precision_recall_fscore_support(y_true, y_pred, average='weighted')
-    precision_macro, recall_macro, f1_macro = precision_recall_fscore_support(y_true, y_pred,average='macro')
+    accuracy=accuracy_score(y_true,y_pred)
+    precision_per_class, recall_per_class, f1_per_class,  support_per_class= precision_recall_fscore_support(y_true, y_pred)
+    precision_weighted, recall_weighted, f1_weighted,support_weighted = precision_recall_fscore_support(y_true, y_pred, average='weighted')
+    precision_macro, recall_macro, f1_macro,support_macro = precision_recall_fscore_support(y_true, y_pred,average='macro')
 
     # Prepare Result
     # TODO Confusion matrix is included in output csv? Formatting of metrics in output file?
-    output_pred = zip(np.arange(y_pred.shape[0]), y_pred)  # In format (y_instance,prediction)
-
+    conf_mx=confusion_matrix(y_true,y_pred)
     #Output to CSV
-    pd.DataFrame(output_pred).to_csv('%s-DS%d.csv' % (model_name, dataset_id), index=False, header=['y_instance', 'class'])
-
-
+    predicted=[np.sum(y_pred==i) for i in range(conf_mx.shape[0])]
+    output_acc=zip([round(accuracy,2)],[round(f1_weighted,2)],[round(f1_macro,2)])
+    output_pred = zip(np.arange(y_pred.shape[0]), predicted,np.round(precision_per_class,2),np.round(recall_per_class,2),np.round(f1_per_class,2))  # In format (y_instance,prediction)
+    df_0=pd.DataFrame(output_acc)
+    df_0.to_csv('results/%s-DS%d.csv' % (model_name, dataset_id), index=False, header=[ 'Accuracy','F1_weighted','F1_macro'])    
+    df_1=pd.DataFrame(output_pred)
+    df_1.to_csv('results/%s-DS%d.csv' % (model_name, dataset_id), index=False, mode='a',header=[ 'Instance','Predicted','Precision','Recall','F1'])
+    df_2=pd.DataFrame(conf_mx,columns=np.arange(conf_mx.shape[1]))
+    df_2.to_csv('results/%s-DS%d.csv' % (model_name, dataset_id), header=True, mode='a')
+   
     return
 
 
@@ -117,8 +124,8 @@ def GNB(train, val):
     gnb.fit(X, Y)
 
     # Get predictions and true labels of dataset2
-    Y_pred = gnb.predict(np.array(df2_val)[:, :1024])
-    Y_true = df2_val[df2_val.columns[-1]]
+    y_pred = gnb.predict(np.array(df2_val)[:, :1024])
+    y_true = df2_val[df2_val.columns[-1]]
 
     # Output predictions and metrics of dataset2 to CSV
     output_metrics_and_csv(y_pred, y_true,'GNB',2)
@@ -127,6 +134,7 @@ def GNB(train, val):
 
 
 def Base_DT(train, val):
+    from sklearn.tree import DecisionTreeClassifier
     """ Description
 
     Parameters
@@ -142,21 +150,29 @@ def Base_DT(train, val):
     df1_val, df2_val = val
 
     # Define Model
-
+    Base_Decision_tree=DecisionTreeClassifier(criterion='entropy')
     # Apply model to dataset1
-
+    X = df1_train[df1_train.columns[:-1]]
+    Y = df1_train[df1_train.columns[-1]]
+    Base_Decision_tree.fit(X,Y)
     # Get predictions and true labels of dataset1
-
+    y_pred = Base_Decision_tree.predict(np.array(df1_val)[:, :1024])
+    y_true = df1_val[df1_val.columns[-1]]
     # Output predictions and metrics of dataset1 to CSV
-
+    output_metrics_and_csv(y_pred, y_true,'Base-DT',1)
     # Apply model to dataset2
-
+    X = df2_train[df2_train.columns[:-1]]
+    Y = df2_train[df2_train.columns[-1]]
+    Base_Decision_tree.fit(X,Y)
     # Get predictions and true labels of dataset2
-
+    y_pred = Base_Decision_tree.predict(np.array(df2_val)[:, :1024])
+    y_true = df2_val[df2_val.columns[-1]]
     # Output predictions and metrics of dataset2 to CSV
+    output_metrics_and_csv(y_pred, y_true,'Base-DT',2)
 
 
 def Best_DT(train, val):
+    from sklearn.tree import DecisionTreeClassifier
     """ Description
 
     Parameters
@@ -172,18 +188,34 @@ def Best_DT(train, val):
     df1_val, df2_val = val
 
     # Define Model
-
-    # Apply model to dataset1
-
+    Best_Decision_tree=DecisionTreeClassifier()
+    param_grid={'criterion':['entropy'],'max_depth':[26,27,28],'min_samples_split':[5],'min_impurity_decrease':[0,0.001],'class_weight':[None]}   
+     # Apply model to dataset1
+    grid_search=GridSearchCV(Best_Decision_tree,param_grid,cv=16,return_train_score=True)
     # Get predictions and true labels of dataset1
-
+    X = df1_train[df1_train.columns[:-1]]
+    Y = df1_train[df1_train.columns[-1]]
+    grid_search.fit(X,Y)
+    final_model=grid_search.best_estimator_
+    print(grid_search.best_estimator_)
+    y_pred = final_model.predict(np.array(df1_val)[:, :1024])
+    y_true = df1_val[df1_val.columns[-1]]
     # Output predictions and metrics of dataset1 to CSV
-
+    output_metrics_and_csv(y_pred, y_true,'Best-DT',1)
     # Apply model to dataset2
-
-    # Get predictions and true labels of dataset2
-
-    # Output predictions and metrics of dataset2 to CSV
+    # X = df2_train[df2_train.columns[:-1]]
+    # Y = df2_train[df2_train.columns[-1]]
+    # param_grid={'criterion':['gini','entropy'],'max_depth':[5,10],'min_samples_split':[2,3,5],'min_impurity_decrease':[0.5,1],'class_weight':[None,'balanced']}   
+    # # Apply model to dataset1
+    # grid_search=GridSearchCV(Best_Decision_tree,param_grid,cv=10,return_train_score=True)
+    # grid_search.fit(X,Y)
+    # final_model=grid_search.best_estimator_
+    # print(grid_search.best_estimator_)
+    # # Get predictions and true labels of dataset2
+    # y_pred = final_model.predict(np.array(df2_val)[:, :1024])
+    # y_true = df2_val[df2_val.columns[-1]]
+    # # Output predictions and metrics of dataset2 to CSV
+    # output_metrics_and_csv(y_pred, y_true,'Best-DT',2)
 
     return
 
@@ -292,10 +324,11 @@ def main():
     df_val = (pd.read_csv('data/val_1.csv'), pd.read_csv('data/val_2.csv'))
 
     # Plot instance distribution
-    plot_data(df_train)
+    #plot_data(df_train)
 
     # Run models
-
+    Best_DT(df_train,df_val)
+    #GNB(df_train,df_val)
     return
 
 
